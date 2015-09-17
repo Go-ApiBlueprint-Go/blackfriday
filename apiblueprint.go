6ee52c480c9c93e4eb097091c2ee79c729a4a842
Ladislav Prskavec <ladislav@prskavec.net>
2015-09-17 20:20:53 +0200

    Basic implementation

--- apiblueprint.go
+++ apiblueprint.go
@@ -36,19 +36,7 @@ func (options *ApiBlueprint) GetFlags() int {
 
 // render code chunks using verbatim, or listings if we have a language
 func (options *ApiBlueprint) BlockCode(out *bytes.Buffer, text []byte, lang string) {
-	if lang == "" {
-		out.WriteString("\n\\begin{verbatim}\n")
-	} else {
-		out.WriteString("\n\\begin{lstlisting}[language=")
-		out.WriteString(lang)
-		out.WriteString("]\n")
-	}
 	out.Write(text)
-	if lang == "" {
-		out.WriteString("\n\\end{verbatim}\n")
-	} else {
-		out.WriteString("\n\\end{lstlisting}\n")
-	}
 }
 
 func (options *ApiBlueprint) TitleBlock(out *bytes.Buffer, text []byte) {
@@ -56,16 +44,12 @@ func (options *ApiBlueprint) TitleBlock(out *bytes.Buffer, text []byte) {
 }
 
 func (options *ApiBlueprint) BlockQuote(out *bytes.Buffer, text []byte) {
-	out.WriteString("\n\\begin{quotation}\n")
 	out.Write(text)
-	out.WriteString("\n\\end{quotation}\n")
 }
 
 func (options *ApiBlueprint) BlockHtml(out *bytes.Buffer, text []byte) {
 	// a pretty lame thing to do...
-	out.WriteString("\n\\begin{verbatim}\n")
 	out.Write(text)
-	out.WriteString("\n\\end{verbatim}\n")
 }
 
 func (options *ApiBlueprint) Header(out *bytes.Buffer, text func() bool, level int, id string) {
@@ -73,49 +57,39 @@ func (options *ApiBlueprint) Header(out *bytes.Buffer, text func() bool, level i
 
 	switch level {
 	case 1:
-		out.WriteString("\n\\section{")
+		out.WriteString("\n#")
 	case 2:
-		out.WriteString("\n\\subsection{")
+		out.WriteString("\n##")
 	case 3:
-		out.WriteString("\n\\subsubsection{")
+		out.WriteString("\n###")
 	case 4:
-		out.WriteString("\n\\paragraph{")
+		out.WriteString("\n")
 	case 5:
-		out.WriteString("\n\\subparagraph{")
+		out.WriteString("\n")
 	case 6:
-		out.WriteString("\n\\textbf{")
+		out.WriteString("\n")
 	}
 	if !text() {
 		out.Truncate(marker)
 		return
 	}
-	out.WriteString("}\n")
+	out.WriteString("\n")
 }
 
 func (options *ApiBlueprint) HRule(out *bytes.Buffer) {
-	out.WriteString("\n\\HRule\n")
+	out.WriteString("\n\n")
 }
 
 func (options *ApiBlueprint) List(out *bytes.Buffer, text func() bool, flags int) {
 	marker := out.Len()
-	if flags&LIST_TYPE_ORDERED != 0 {
-		out.WriteString("\n\\begin{enumerate}\n")
-	} else {
-		out.WriteString("\n\\begin{itemize}\n")
-	}
 	if !text() {
 		out.Truncate(marker)
 		return
 	}
-	if flags&LIST_TYPE_ORDERED != 0 {
-		out.WriteString("\n\\end{enumerate}\n")
-	} else {
-		out.WriteString("\n\\end{itemize}\n")
-	}
 }
 
 func (options *ApiBlueprint) ListItem(out *bytes.Buffer, text []byte, flags int) {
-	out.WriteString("\n\\item ")
+	out.WriteString("\n ")
 	out.Write(text)
 }
 
@@ -130,27 +104,17 @@ func (options *ApiBlueprint) Paragraph(out *bytes.Buffer, text func() bool) {
 }
 
 func (options *ApiBlueprint) Table(out *bytes.Buffer, header []byte, body []byte, columnData []int) {
-	out.WriteString("\n\\begin{tabular}{")
-	for _, elt := range columnData {
-		switch elt {
-		case TABLE_ALIGNMENT_LEFT:
-			out.WriteByte('l')
-		case TABLE_ALIGNMENT_RIGHT:
-			out.WriteByte('r')
-		default:
-			out.WriteByte('c')
-		}
-	}
-	out.WriteString("}\n")
+	out.WriteString("\n")
+	out.WriteString("\n")
 	out.Write(header)
-	out.WriteString(" \\\\\n\\hline\n")
+	out.WriteString("\n\n")
 	out.Write(body)
-	out.WriteString("\n\\end{tabular}\n")
+	out.WriteString("\n\n")
 }
 
 func (options *ApiBlueprint) TableRow(out *bytes.Buffer, text []byte) {
 	if out.Len() > 0 {
-		out.WriteString(" \\\\\n")
+		out.WriteString(" \n")
 	}
 	out.Write(text)
 }
@@ -179,74 +143,45 @@ func (options *ApiBlueprint) FootnoteItem(out *bytes.Buffer, name, text []byte, 
 }
 
 func (options *ApiBlueprint) AutoLink(out *bytes.Buffer, link []byte, kind int) {
-	out.WriteString("\\href{")
-	if kind == LINK_TYPE_EMAIL {
-		out.WriteString("mailto:")
-	}
 	out.Write(link)
-	out.WriteString("}{")
-	out.Write(link)
-	out.WriteString("}")
 }
 
 func (options *ApiBlueprint) CodeSpan(out *bytes.Buffer, text []byte) {
-	out.WriteString("\\texttt{")
 	escapeSpecialChars(out, text)
-	out.WriteString("}")
 }
 
 func (options *ApiBlueprint) DoubleEmphasis(out *bytes.Buffer, text []byte) {
-	out.WriteString("\\textbf{")
 	out.Write(text)
-	out.WriteString("}")
 }
 
 func (options *ApiBlueprint) Emphasis(out *bytes.Buffer, text []byte) {
-	out.WriteString("\\textit{")
+	out.WriteString("*")
 	out.Write(text)
-	out.WriteString("}")
+	out.WriteString("*")
 }
 
 func (options *ApiBlueprint) Image(out *bytes.Buffer, link []byte, title []byte, alt []byte) {
-	if bytes.HasPrefix(link, []byte("http://")) || bytes.HasPrefix(link, []byte("https://")) {
-		// treat it like a link
-		out.WriteString("\\href{")
-		out.Write(link)
-		out.WriteString("}{")
-		out.Write(alt)
-		out.WriteString("}")
-	} else {
-		out.WriteString("\\includegraphics{")
-		out.Write(link)
-		out.WriteString("}")
-	}
+	out.Write(link)
 }
 
 func (options *ApiBlueprint) LineBreak(out *bytes.Buffer) {
-	out.WriteString(" \\\\\n")
+	out.WriteString("\n")
 }
 
 func (options *ApiBlueprint) Link(out *bytes.Buffer, link []byte, title []byte, content []byte) {
-	out.WriteString("\\href{")
 	out.Write(link)
-	out.WriteString("}{")
 	out.Write(content)
-	out.WriteString("}")
 }
 
 func (options *ApiBlueprint) RawHtmlTag(out *bytes.Buffer, tag []byte) {
 }
 
 func (options *ApiBlueprint) TripleEmphasis(out *bytes.Buffer, text []byte) {
-	out.WriteString("\\textbf{\\textit{")
 	out.Write(text)
-	out.WriteString("}}")
 }
 
 func (options *ApiBlueprint) StrikeThrough(out *bytes.Buffer, text []byte) {
-	out.WriteString("\\sout{")
 	out.Write(text)
-	out.WriteString("}")
 }
 
 // TODO: this
@@ -254,35 +189,35 @@ func (options *ApiBlueprint) FootnoteRef(out *bytes.Buffer, ref []byte, id int) 
 
 }
 
-func needsBackslash(c byte) bool {
-	for _, r := range []byte("_{}%$&\\~#") {
-		if c == r {
-			return true
-		}
-	}
-	return false
-}
+// func needsBackslash(c byte) bool {
+// 	for _, r := range []byte("_{}%$&\\~#") {
+// 		if c == r {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
 
-func escapeSpecialChars(out *bytes.Buffer, text []byte) {
-	for i := 0; i < len(text); i++ {
-		// directly copy normal characters
-		org := i
+// func escapeSpecialChars(out *bytes.Buffer, text []byte) {
+// 	for i := 0; i < len(text); i++ {
+// 		// directly copy normal characters
+// 		org := i
 
-		for i < len(text) && !needsBackslash(text[i]) {
-			i++
-		}
-		if i > org {
-			out.Write(text[org:i])
-		}
+// 		for i < len(text) && !needsBackslash(text[i]) {
+// 			i++
+// 		}
+// 		if i > org {
+// 			out.Write(text[org:i])
+// 		}
 
-		// escape a character
-		if i >= len(text) {
-			break
-		}
-		out.WriteByte('\\')
-		out.WriteByte(text[i])
-	}
-}
+// 		// escape a character
+// 		if i >= len(text) {
+// 			break
+// 		}
+// 		out.WriteByte('\\')
+// 		out.WriteByte(text[i])
+// 	}
+// }
 
 func (options *ApiBlueprint) Entity(out *bytes.Buffer, entity []byte) {
 	// TODO: convert this into a unicode character or something
@@ -295,34 +230,21 @@ func (options *ApiBlueprint) NormalText(out *bytes.Buffer, text []byte) {
 
 // header and footer
 func (options *ApiBlueprint) DocumentHeader(out *bytes.Buffer) {
-	out.WriteString("\\documentclass{article}\n")
-	out.WriteString("\n")
-	out.WriteString("\\usepackage{graphicx}\n")
-	out.WriteString("\\usepackage{listings}\n")
-	out.WriteString("\\usepackage[margin=1in]{geometry}\n")
-	out.WriteString("\\usepackage[utf8]{inputenc}\n")
-	out.WriteString("\\usepackage{verbatim}\n")
-	out.WriteString("\\usepackage[normalem]{ulem}\n")
-	out.WriteString("\\usepackage{hyperref}\n")
-	out.WriteString("\n")
-	out.WriteString("\\hypersetup{colorlinks,%\n")
-	out.WriteString("  citecolor=black,%\n")
-	out.WriteString("  filecolor=black,%\n")
-	out.WriteString("  linkcolor=black,%\n")
-	out.WriteString("  urlcolor=black,%\n")
-	out.WriteString("  pdfstartview=FitH,%\n")
-	out.WriteString("  breaklinks=true,%\n")
-	out.WriteString("  pdfauthor={Blackfriday Markdown Processor v")
+	out.WriteString("\nSTART\n")
 	out.WriteString(VERSION)
-	out.WriteString("}}\n")
-	out.WriteString("\n")
-	out.WriteString("\\newcommand{\\HRule}{\\rule{\\linewidth}{0.5mm}}\n")
-	out.WriteString("\\addtolength{\\parskip}{0.5\\baselineskip}\n")
-	out.WriteString("\\parindent=0pt\n")
-	out.WriteString("\n")
-	out.WriteString("\\begin{document}\n")
+	out.WriteString("\n\n")
 }
 
 func (options *ApiBlueprint) DocumentFooter(out *bytes.Buffer) {
-	out.WriteString("\n\\end{document}\n")
+	out.WriteString("\nEND\n")
+}
+
+// ApiBlueprintBasic is a convenience function for simple rendering.
+func ApiBlueprintBasic(input []byte) []byte {
+	// set up the HTML renderer
+	htmlFlags := HTML_USE_XHTML
+	renderer := ApiBlueprintRenderer(htmlFlags)
+
+	// set up the parser
+	return MarkdownOptions(input, renderer, Options{Extensions: 0})
 }
